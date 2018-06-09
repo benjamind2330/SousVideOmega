569340da33d574c17fdcf50130e704e2a04cb706
Ben Dart <benjamin.d2330@gmail.com>
2018-06-09 11:51:42 +0200

    Added simple threaded watchdog module skeleton.

--- OmegaBackend/src/Safety/WatchDog.py
+++ OmegaBackend/src/Safety/WatchDog.py
@@ -1,14 +1,15 @@
-def test():
-    print("test")
+import time
+import threading
 
-'''
-class Watchdog:
+class Watchdog (threading.Thread):
 
-    def __init__(self):
-            self.i = 0
+    def __init__(self, delay):
+        threading.Thread.__init__(self)
+        self.i = 0
+        self.delay = delay
 
-    def run():
-        while (true):
-            print("Running ... Looped " + i + " times")
-            i = i + 1
-'''
+    def run(self):
+        while (True):
+            print("Running ... Looped " + str(self.i) + " times")
+            self.i = self.i + 1
+            time.sleep(self.delay)
--- OmegaBackend/src/main.py
+++ OmegaBackend/src/main.py
@@ -1,8 +1,3 @@
-import WatchDog
-'''
-watchdog = Watchdog()
-
-watchdog.run()
-'''
-
-WatchDog.test()
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
+from Safety import WatchDog
